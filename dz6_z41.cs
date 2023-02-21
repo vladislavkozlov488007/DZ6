094f711f9c3ea229d0d2abb0a717300fc992ce3b
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-02-21 09:30:09 +0300

    Add files via upload

---
+++ dz6_z41.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2; 1, -7, 567, 89, 223-> 3;
+
+System.Console.WriteLine("Введите размер массива");
+int[] array = new int [Convert.ToInt32(Console.ReadLine())];
+int count = 0;
+for (int i=0; i<array.Length; i++)
+    {
+        System.Console.WriteLine("Введите числа");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0) count++;
+    }
+
+System.Console.Write($"Количество положительных чисел равно {count}");
+
---
+++ dz6_z43.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("Введите значения b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите значения k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите значения b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите значения k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x;
+double y;
+
+try {
+if(k1 != k2)
+{
+    x = ((b1 - b2) / (k2 - k1));
+    y = k1 * x + b1;
+    Console.WriteLine($"Точки пересечения двух прямых [{x}; {y}]");
+} 
+else if (k1 == k2) System.Console.WriteLine("Прямые совпадают или параллельны"); }
+catch {
+System.Console.WriteLine("Точек пересечения нет"); }
